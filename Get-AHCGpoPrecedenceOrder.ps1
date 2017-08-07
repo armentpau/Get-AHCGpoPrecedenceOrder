a287ed6f797a578e9c3b21b15b64debcdc6207c2
Paul DeArment <paul.dearmentjr@aurora.org>
2017-08-07 15:21:15 -0500

    Server will always be populated now - removing looking for null server

--- Get-AHCGpoPrecedenceOrder.ps1
+++ Get-AHCGpoPrecedenceOrder.ps1
@@ -17,7 +17,9 @@
 		If you do not specify the name by using the Server parameter, a random server is selected
 	
 	.PARAMETER Domain
-		A description of the Domain parameter.
+		Specifies the domain for this cmdlet. You must specify the fully qualified domain name (FQDN) of the domain (for example: sales.contoso.com).
+
+For the Get-GPInheritance cmdlet, this is typically the domain of the container (domain or OU) for which you want to retrieve inheritance information. If the specified domain is different than the domain of the container, a trust must exist between the two domains.
 	
 	.EXAMPLE
 		PS C:\> Get-AHCGpoPrecedenceOrder -Target 'Value1'
@@ -68,35 +70,17 @@ function Get-AHCGpoPrecedenceOrder
 		{
 			Write-Verbose $ouTarget
 			$TargetSplitArray = $ouTarget.tostring().tolower() -split ","
-			if ([string]::IsNullOrEmpty($Server))
-			{
-				$domainName = "dc=$((Get-ADDomain).name.tostring().tolower())"
-			}
-			else
-			{
-				$domainName = "dc=$((Get-ADDomain -Server $Server).name.tostring().tolower())"
-			}
+			$domainName = "dc=$((Get-ADDomain -Server $Server).name.tostring().tolower())"
 			$domainIndex = [array]::IndexOf($TargetSplitArray, $domainName)
 			$OUArray = @()
 			$counter = 0
 			do
 			{
 				Write-Verbose "Working with ou $($($TargetSplitArray[$counter .. $(($TargetSplitArray | measure-object).count - 1)] -join ","))"
-				if ([string]::IsNullOrEmpty($Server))
-				{
-					$OUArray += New-Object -TypeName System.Management.Automation.PSObject -Property @{
-						"Order"	      = $counter + 1
-						"OUData"	  = Get-ADobject -Identity $($TargetSplitArray[$counter .. $(($TargetSplitArray | measure-object).count - 1)] -join ",")
-						"Links"	      = Get-GPInheritance -Target $($TargetSplitArray[$counter .. $(($TargetSplitArray | measure-object).count - 1)] -join ",")
-					}
-				}
-				else
-				{
-					$OUArray += New-Object -TypeName System.Management.Automation.PSObject -Property @{
-						"Order"	       = $counter + 1
-						"OUData"	   = Get-ADobject -Identity $($TargetSplitArray[$counter .. $(($TargetSplitArray | measure-object).count - 1)] -join ",") -Server $Server
-						"Links"	       = Get-GPInheritance -Target $($TargetSplitArray[$counter .. $(($TargetSplitArray | measure-object).count - 1)] -join ",") -Server $Server
-					}
+				$OUArray += New-Object -TypeName System.Management.Automation.PSObject -Property @{
+					"Order"	       = $counter + 1
+					"OUData"	   = Get-ADobject -Identity $($TargetSplitArray[$counter .. $(($TargetSplitArray | measure-object).count - 1)] -join ",") -Server $Server
+					"Links"	       = Get-GPInheritance -Target $($TargetSplitArray[$counter .. $(($TargetSplitArray | measure-object).count - 1)] -join ",") -Server $Server
 				}
 				$counter++
 			}
